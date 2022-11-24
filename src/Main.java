ba8ef2ecb82e087e0a18dcb1a573edfdd0f5602c
agarkov.ra <rodionagarkov@gmail.com>
2022-11-24 14:27:56 +0300

    fixed code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        int counterForThree = 0;
-        int counterForFive = 0;
+        int counter = 0;
 
-        for (int i = 0; i < 1000; i += 3) {
-            counterForThree = counterForThree + i;
+        for (int i = 0; i < 1000; i += 3, i +=5) {
+            counter = counter + i;
+
         }
-        for (int b = 0; b < 1000; b += 5){
-            counterForFive = counterForFive + b;
-        }
-        System.out.println(counterForThree + counterForFive);
+        System.out.println(counter);
     }
 }
